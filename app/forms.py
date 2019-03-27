dd69a712b8d0aa8ad98ed4e4a63e51b14fa2b361
Kayla Effs <kaylaeffs@gmail.com>
2019-03-27 16:47:39 +0000

    exexrcise one

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import Form, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, InputRequired, Length
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 
+class UploadForm(FlaskForm):
+    description = TextAreaField('Descriptioon', validators=[
+        InputRequired(),
+        Length(min=15, max=200, message="Can enter from 15 to 200 characters.")
+    ])
+    photo = FileField('Image', validators=[
+        FileRequired("You need a profile pic."), 
+        FileAllowed(['jpg','png'], 'Images only!')
+    ])
+    submit = SubmitField("Add Profile")
